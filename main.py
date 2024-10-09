1d24c3dc27f47ef143fb335f76a0eb1da4344cce
wyattcasss <58782405+wyattcasss@users.noreply.github.com>
2024-10-09 01:18:46 -0400

    Day one work
    Cleaned data, created predictors, and started an initial model. Predicting the amount of wins by elimination rounds.

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-import pandas 
+import pandas as pd
+matches = pd.read_csv("win_loss_methods_count (1).csv", index_col=0)
+futureMatches =pd.read_csv("win_loss_methods_count (1).csv", index_col=0)
+matches["map_code"]=matches["Map"].astype("category").cat.codes
+matches["target"] =(matches["Elimination"])
+futureMatches["map_code"]=futureMatches["Map"].astype("category").cat.codes
+futureMatches["target"] =(futureMatches["Elimination"])
+from sklearn.ensemble import RandomForestClassifier
+rf = RandomForestClassifier(n_estimators=50, min_samples_split=10, random_state=1)
+train = matches 
+test = futureMatches 
+predictors = ["map_code",
+              "Time Expiry (No Plant)",
+              "Eliminated","Defused Failed",
+              "Detonation Denied","Time Expiry (Failed to Plant)"]
+rf.fit(train[predictors],train["target"]) 
+preds=rf.predict(test[predictors])
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(test["target"],preds)
+print(acc)
